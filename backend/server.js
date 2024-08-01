3ef924fbc6e18914715689a0efa46dcc6ed40f7d
Kaan Kani İnce <kkince@newmind.ai>
2024-08-01 11:57:57 +0300

    Routes file created, sidebar added

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const Routes = require('./routes');
-const port = 5000;
+const Routes = require('./routes/routes');
+const PORT = 5000;
 
 app.use(cors());
 app.use(express.json());
 
 // Try app.use('/')
-app.use('/school', Routes)
+app.use('/', Routes)
 
-app.listen(port, () => console.log(`app listening on port ${port}`));
+app.listen(PORT, () => {
+    console.log(`Server started at port no. ${PORT}`)
+})
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
-import {useState} from 'react'
 import './App.css'
 import Home from "./components/Home/Home.jsx";
 import {Route, Routes} from "react-router-dom";
 import Student from "./components/LoginForm/student/student.jsx";
 import Teacher from "./components/LoginForm/teacher/teacher.jsx";
 import Registration from "./components/RegisterForm/registration.jsx";
+import SideBar from "./components/SideBar/sidebar.jsx";
 
 function App() {
 
@@ -14,6 +14,7 @@ function App() {
             <Route path="/student" element={<Student/>}/>
             <Route path="/teacher" element={<Teacher/>}/>
             <Route path="/register" element={<Registration/>}/>
+            <Route path="/sidebar" element={<SideBar/>}/>
         </Routes>
     )
 }
